4dadf3f22b0c7fe2e542ade9f52fc061c43bd0cf
Kamaluddin Arsyad Fadllillah <102853169+d-arsya@users.noreply.github.com>
2026-02-19 06:15:50 +0700

    fix: Commas

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 3000),
-  url: env('PUBLIC_URL', 'http://localhost:3000')
+  url: env('PUBLIC_URL', 'http://localhost:3000'),
   //(env('NODE_ENV', 'production') === 'production' ? '/cms' : ''),
 
   app: {
